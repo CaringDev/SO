fd802a4b08e9e21e23eecae1bb91013807a573a0
Raphael Schweizer <rschweizer@schweizer-informatik.ch>
2015-08-02 21:05:57 +0200

    initial

---
+++ F/F.fs
@@ -0,0 +1,6 @@
+﻿module Test
+ open Microsoft.FSharp.Data.TypeProviders
+ type internal DB = SqlDataConnection<"Data Source=.;Initial Catalog=OBSCURED;Integrated Security=SSPI;">
+ let private entities = DB.GetDataContext().OBSCURED
+
+ type dbml = DbmlFile<"MyDatabase.dbml", ContextTypeName = "MyDataContext">
