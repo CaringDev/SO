e464f31da42509465a5e47b7ce38d3ce2280dfe1
Raphael Schweizer <rschweizer@schweizer-informatik.ch>
2015-08-09 19:45:54 +0200

    WiaCopyTool

---
+++ WiaCopyTool/WiaCopyTool.cs
@@ -0,0 +1,21 @@
+﻿namespace WiaCopyTool
+{
+    using System;
+    using System.IO;
+
+    public static class WiaCopyTool
+    {
+        public static void Main()
+        {
+            var savePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "Auto Import");
+
+            foreach (var iPhone in WiaCopy.GetAppleDevices())
+            {
+                foreach (var imgItem in WiaCopy.GetImgItems(iPhone))
+                {
+                    WiaCopy.TransferItem(imgItem, Path.Combine(savePath, iPhone.Properties["Name"].get_Value()));
+                }
+            }
+        }
+    }
+}
